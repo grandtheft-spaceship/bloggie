fe17cac906f7c96948399261f68bdcdc94c3aefb
grandtheft-spacehip <justdizon71@gmail.com>
2016-05-31 11:06:47 -0700

    Created Post controller/view and added new blog functionality. Updated post column to body

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160529122618) do
+ActiveRecord::Schema.define(version: 20160530121601) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
-    t.text     "post"
+    t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
