00b88847cfa9d31fbe1fc2c3d2f844e3fe6af867
grandtheft-spacehip <justdizon71@gmail.com>
2016-06-05 09:34:22 -0700

    Added Edit/Update/Destroy functionality to blog posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,20 +1,39 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.order("created_at")
+    @posts = Post.order("created_at DESC")
   end
 
   def new
+    @post = Post.new
   end
 
   def create
     @post = Post.create(post_params)
 
-    redirect_to @post
+    redirect_to root_path
   end
 
   def show
     @post = Post.find(params[:id])
   end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update_attributes(post_params)
+
+    redirect_to root_path
+  end
+
+def destroy
+  @post = Post.find(params[:id])
+  @post.destroy
+
+  redirect_to root_path
+end
 
   private
 
