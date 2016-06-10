cfd03abe05a556a37d2470ee94cda3fb922e59c6
grandtheft-spacehip <justdizon71@gmail.com>
2016-06-09 19:42:30 -0700

    Refactored logic to find post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+  before_action :find_post, except: [:index, :new, :create]
   def index
     @posts = Post.order("created_at DESC")
   end
@@ -15,30 +16,30 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
     @post.update_attributes(post_params)
 
     redirect_to root_path
   end
 
-def destroy
-  @post = Post.find(params[:id])
-  @post.destroy
+  def destroy
+    @post.destroy
 
-  redirect_to root_path
-end
+    redirect_to root_path
+  end
 
   private
 
   def post_params
     params.require(:post).permit(:title, :body)
+  end
+
+  def find_post
+    @post = Post.find(params[:id])
   end
 end
